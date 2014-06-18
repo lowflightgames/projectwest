62ac172c02babc4f9f982f3c7e33affe75679bb4
Michał Piekarski <michal.piekarski@icloud.com>
2014-06-18 03:58:13 +0200

    Pierwsze testy nawigacji w budynkach (#13183451040239)

---
+++ Assets/Scripts/ChangeTargetScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeTargetScript : MonoBehaviour
+{
+
+		public Transform[] Targets;
+		private int targetID = 0;
+		private AICharacterControl AICOntrolScript;
+	    
+		void Start ()
+		{
+				AICOntrolScript = gameObject.GetComponent<AICharacterControl> ();
+		}
+
+		// Update is called once per frame
+		void Update ()
+		{
+				if (Input.GetKeyDown (KeyCode.N)) {
+						if (targetID < Targets.Length - 1) {
+								++targetID;
+						} else {
+								targetID = 0;
+						}
+						AICOntrolScript.target = Targets [targetID];
+				}
+		}
+}
